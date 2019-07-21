d11d8d16629d35dc5b7303fe5ecff04eec3603d2
Franky Liu <cuifranky95@gmail.com>
2019-07-21 03:50:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,7 +7,7 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if (age <=19 && age >=13) {
-    return "You are a teenage!"
+    return "You are a teenager!!"
     
   } else {
     if (age >19 && age <13) {
